a03b04aeda972fa1ac8c5b02eb85807ab7088891
Thaabiet Salaam <thaabiet.salaam@flash.co.za>
2022-12-15 10:09:00 +0200

    tainted commit

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -6,9 +6,30 @@
         {
             Console.WriteLine("Hello, World!");
 
-            
+            //Collapsible "if" statements should be merged
+            int one = 1;
+            int two = 2;
+
+            if (one != 0)
+            {
+                if (one == 1)
+                {
+                    Console.WriteLine(one);
+                }
+            }
 
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Unused local variables should be removed
+        /// </summary>
+        /// <param name="hours"></param>
+        /// <returns></returns>
+        public int LocalVariables(int hours)
+        {
+            int seconds = 0;   // seconds is never used
+            return hours * 60;
         }
     }
 }
